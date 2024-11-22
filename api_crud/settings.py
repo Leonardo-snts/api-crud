4cba18efa71708a47a80a9ce9c113f30bb524669
Leonardo-snts <leonardosantos.ms77@gmail.com>
2024-11-21 22:06:34 -0400

    feat: adicionando cors dominio render

--- api_crud/settings.py
+++ api_crud/settings.py
@@ -30,13 +30,11 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    '18.228.11.240', # IP público API
-    '15.228.164.101', # IP publico Frontend
-    'crud-frontend-inky-one.vercel.app' # Dominio Vercel
+    'crud-frontend-inky-one.vercel.app', # Dominio Vercel
+    'api-crud-96xx.onrender.com', # Dominio Render
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://15.228.164.101:3000',  # IP publico Frontend
     'http://localhost:3000',       # Para testes locais
     'https://crud-frontend-inky-one.vercel.app', # Dominio Vercel
 ]
